0b8d3a8b806eaf0b829ce38e95272a29066c2dd5
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-24 11:11:55 +0100

    Add Modal Selector

--- character.js
+++ character.js
@@ -1,5 +1,10 @@
 var firstName = "Frost";
+var firstRace = "Vampire";
+var firstItem = "Sword";
 var secondName = "Abbath";
+var secondRace = "Orc";
+var secondItem = "Axe";
+
 var log = document.getElementById("log");
 var logtxt = "Just another day at Wacken Open Air.";
 log.innerHTML = logtxt;
@@ -39,3 +44,34 @@ document.getElementById("secondYield").addEventListener("click", () => {
     log.innerHTML = logtxt;
     document.getElementById("logContainer").scrollTop = 9999999;
 });
+
+
+// MODAL SELECTOR //
+
+var modal = document.getElementById("selector");
+
+window.onload = function() {
+   modal.style.display = "block";
+}
+
+document.getElementById("firstChoose").addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+fight.onclick = function() {
+    firstName = document.getElementById("firstName").value;
+    firstRace = document.getElementById("firstRace").value;
+    firstItem = document.getElementById("firstItem").value;
+    document.getElementById("firstNameDisplay").innerHTML = "Hero: " + firstName;
+    document.getElementById("firstRaceDisplay").innerHTML = "Race: " + firstRace;
+    document.getElementById("firstItemDisplay").innerHTML = "Special item: "+ firstItem;
+    document.getElementById("secondNameDisplay").innerHTML = "Enemy: " + secondName;
+    document.getElementById("secondRaceDisplay").innerHTML = "Race: " + secondRace;
+    document.getElementById("secondItemDisplay").innerHTML = "Special item: " + secondItem;
+    logtxt = "Just another day at Wacken Open Air.";
+    log.innerHTML = logtxt;
+    logtxt = logtxt + ("<br>" + firstName + " the " + firstRace + " will fight " + secondName + " the " + secondRace + "!");
+    log.innerHTML = logtxt;
+    document.getElementById("logContainer").scrollTop = 9999999;
+    modal.style.display = "none";
+ }
