c1177c3e89bac466c077ee89b934161814337aa5
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-02-24 16:42:31 +0100

    Add Initial Random Enemy

--- character.js
+++ character.js
@@ -1,18 +1,144 @@
-// INITIAL VALUES //
-var heroName = "Frost";
-var heroRace = "Vampire";
-var heroItem = "Sword";
-var hero = new Person(heroName, heroRace, heroItem);
+// Use this script to generate your character //
+function Person(name, race, item) {
+    this.name = name;
+    this.race = race;
+    this.item = item;
+    this.currentHealth = 100;
+    this.maxHealth = 100;
+    this.min = 3;
+    this.maxDamage = 20;
+    this.maxHealing = 30;
 
-var enemyName = "Abbath";
-var enemyRace = "Orc";
-var enemyItem = "Axe";
-var enemy = new Person(enemyName, enemyRace, enemyItem);
+    this.heal = function(){};
 
-var log = document.getElementById("log");
-var logtxt = "Just another day at Wacken Open Air.";
-log.innerHTML = logtxt;
+    this.damage = function(){};
 
+    this.totalDamage = this.damage();
+
+    this.displayChar = function(){
+        return console.log(`I am ${this.name} the ${this.race}! I wield a ${this.item} and my total health points are ${this.maxHealth}`);
+    };
+}
+
+// MODAL SELECTORS //
+var modalHero = document.getElementById("modalHero");
+var modalEnemy = document.getElementById("modalEnemy");
+
+// SHOW HERO MODAL SELECTOR ON LOAD PAGE AND SETS RANDOM ENEMY //
+window.onload = function() { 
+    modalHero.style.display = "block";
+    randomEnemy();
+    changeEnemy();
+    enemyName = document.getElementById("enemyName").value;
+    enemyRace = document.getElementById("enemyRace").value;
+    enemyItem = document.getElementById("enemyItem").value;
+    enemy = new Person(enemyName, enemyRace, enemyItem);
+    enemy.displayChar();
+    document.getElementById("enemyNameDisplay").innerHTML = "Enemy: " + enemy.name;
+    document.getElementById("enemyRaceDisplay").innerHTML = "Race: " + enemy.race;
+    document.getElementById("enemyItemDisplay").innerHTML = "Special item: " + enemy.item;
+    document.getElementById("enemyHPDisplay").innerHTML = "HP: "+ enemy.currentHealth + "/" + enemy.maxHealth;
+    document.getElementById("enemyHP").value = enemy.currentHealth;
+    document.getElementById("enemyHP").max = enemy.maxHealth;
+}
+
+// SHOW HERO MODAL SELECTOR ON BUTTON CLICK //
+document.getElementById("heroChoose").addEventListener("click", () => {
+    modalHero.style.display = "block";
+});
+
+// SET VALUES AFTER CHOOSING HERO //
+document.getElementById("fightHero").onclick = function() {
+    heroName = document.getElementById("heroName").value;
+    heroRace = document.getElementById("heroRace").value;
+    heroItem = document.getElementById("heroItem").value;
+    hero = new Person(heroName, heroRace, heroItem);
+    hero.displayChar();
+    document.getElementById("heroNameDisplay").innerHTML = "Hero: " + hero.name;
+    document.getElementById("heroRaceDisplay").innerHTML = "Race: " + hero.race;
+    document.getElementById("heroItemDisplay").innerHTML = "Special item: "+ hero.item;
+    document.getElementById("heroHPDisplay").innerHTML = "HP: "+ hero.currentHealth + "/" + hero.maxHealth;
+    document.getElementById("heroHP").value = hero.currentHealth;
+    document.getElementById("heroHP").max = hero.maxHealth;
+    logtxt = "Just another day at Wacken Open Air.";
+    log.innerHTML = logtxt;
+    logtxt = logtxt + ("<br>" + hero.name + " the " + hero.race + " will fight " + enemy.name + " the " + enemy.race + "!");
+    log.innerHTML = logtxt;
+    document.getElementById("logContainer").scrollTop = 9999999;
+    modalHero.style.display = "none";
+ }
+
+ // SHOW ENEMY MODAL SELECTOR ON BUTTON CLICK //
+ document.getElementById("enemyChoose").addEventListener("click", () => {
+    modalEnemy.style.display = "block";
+});
+
+// SET VALUES AFTER CHOOSING ENEMY //
+document.getElementById("fightEnemy").onclick = function() {
+    enemyName = document.getElementById("enemyName").value;
+    enemyRace = document.getElementById("enemyRace").value;
+    enemyItem = document.getElementById("enemyItem").value;
+    enemy = new Person(enemyName, enemyRace, enemyItem);
+    enemy.displayChar();
+    document.getElementById("enemyNameDisplay").innerHTML = "Enemy: " + enemy.name;
+    document.getElementById("enemyRaceDisplay").innerHTML = "Race: " + enemy.race;
+    document.getElementById("enemyItemDisplay").innerHTML = "Special item: " + enemy.item;
+    document.getElementById("enemyHPDisplay").innerHTML = "HP: "+ enemy.currentHealth + "/" + enemy.maxHealth;
+    document.getElementById("enemyHP").value = enemy.currentHealth;
+    document.getElementById("enemyHP").max = enemy.maxHealth;
+    logtxt = "Just another day at Wacken Open Air.";
+    log.innerHTML = logtxt;
+    logtxt = logtxt + ("<br>" + hero.name + " the " + hero.race + " will fight " + enemy.name + " the " + enemy.race + "!");
+    log.innerHTML = logtxt;
+    document.getElementById("logContainer").scrollTop = 9999999;
+    modalEnemy.style.display = "none";
+ }
+
+// FIX ENEMY SELECTOR VALUES AFTER CHOOSING ENEMY //
+changeEnemy = () => {
+    switch(document.getElementById("enemyName").value) {
+        case "Varg Vikernes":
+            document.getElementById("enemyRace").value = "Human";
+            document.getElementById("enemyItem").value = "Staff";
+            document.getElementById("enemyPhoto").src = "assets/enemy-varg-human-rest.jpg";
+            break;
+        case "Abbath":
+            document.getElementById("enemyRace").value = "Orc";
+            document.getElementById("enemyItem").value = "Sword";
+            document.getElementById("enemyPhoto").src = "assets/enemy-abbath-orc-rest.jpg";
+            break;
+            case "Aerendir":
+            document.getElementById("enemyRace").value = "Elf";
+            document.getElementById("enemyItem").value = "Bow";
+            document.getElementById("enemyPhoto").src = "assets/enemy-aerendir-elf-rest.jpg";
+            break;
+        case "Frost":
+            document.getElementById("enemyRace").value = "Vampire";
+            document.getElementById("enemyItem").value = "Boots";
+            document.getElementById("enemyPhoto").src = "assets/enemy-frost-vampire-rest.jpg";
+            break;
+      }
+}
+
+// RANDOMIZE FIRST ENEMY //
+randomEnemy = () => {
+    var random = Math.floor(Math.random()*4);
+    console.log(random);
+    switch(random) {
+        case 0:
+            document.getElementById("enemyName").value = "Varg Vikernes";
+            break;
+        case 1:
+            document.getElementById("enemyName").value = "Abbath";
+            break;
+        case 2:
+            document.getElementById("enemyName").value = "Aerendir";
+            break;
+        case 3:
+            document.getElementById("enemyName").value = "Frost";
+            break;
+      }
+}
 
 // BUTTON HANDLERS //
 document.getElementById("heroAttack").addEventListener("click", () => {
@@ -50,113 +176,3 @@ document.getElementById("enemyYield").addEventListener("click", () => {
     log.innerHTML = logtxt;
     document.getElementById("logContainer").scrollTop = 9999999;
 });
-
-
-// MODAL SELECTORS //
-var modalHero = document.getElementById("modalHero");
-
-window.onload = function() {
-   modalHero.style.display = "block";
-}
-
-document.getElementById("heroChoose").addEventListener("click", () => {
-    modalHero.style.display = "block";
-});
-
-document.getElementById("fightHero").onclick = function() {
-    heroName = document.getElementById("heroName").value;
-    heroRace = document.getElementById("heroRace").value;
-    heroItem = document.getElementById("heroItem").value;
-    hero = new Person(heroName, heroRace, heroItem);
-    document.getElementById("heroNameDisplay").innerHTML = "Hero: " + hero.name;
-    document.getElementById("heroRaceDisplay").innerHTML = "Race: " + hero.race;
-    document.getElementById("heroItemDisplay").innerHTML = "Special item: "+ hero.item;
-    document.getElementById("heroHPDisplay").innerHTML = "HP: "+ hero.currentHealth + "/" + hero.maxHealth;
-    document.getElementById("heroHP").value = hero.currentHealth;
-    document.getElementById("heroHP").max = hero.maxHealth;
-    enemy = new Person(enemyName, enemyRace, enemyItem);
-    document.getElementById("enemyNameDisplay").innerHTML = "Enemy: " + enemy.name;
-    document.getElementById("enemyRaceDisplay").innerHTML = "Race: " + enemy.race;
-    document.getElementById("enemyItemDisplay").innerHTML = "Special item: " + enemy.item;
-    document.getElementById("enemyHPDisplay").innerHTML = "HP: "+ enemy.currentHealth + "/" + enemy.maxHealth;
-    document.getElementById("enemyHP").value = enemy.currentHealth;
-    document.getElementById("enemyHP").max = enemy.maxHealth;
-    logtxt = "Just another day at Wacken Open Air.";
-    log.innerHTML = logtxt;
-    logtxt = logtxt + ("<br>" + hero.name + " the " + hero.race + " will fight " + enemy.name + " the " + enemy.race + "!");
-    log.innerHTML = logtxt;
-    document.getElementById("logContainer").scrollTop = 9999999;
-    modalHero.style.display = "none";
- }
-
- var modalEnemy = document.getElementById("modalEnemy");
-
- document.getElementById("enemyChoose").addEventListener("click", () => {
-    modalEnemy.style.display = "block";
-});
-
-document.getElementById("fightEnemy").onclick = function() {
-    enemyName = document.getElementById("enemyName").value;
-    enemyRace = document.getElementById("enemyRace").value;
-    enemyItem = document.getElementById("enemyItem").value;
-    enemy = new Person(enemyName, enemyRace, enemyItem);
-    document.getElementById("enemyNameDisplay").innerHTML = "Enemy: " + enemy.name;
-    document.getElementById("enemyRaceDisplay").innerHTML = "Race: " + enemy.race;
-    document.getElementById("enemyItemDisplay").innerHTML = "Special item: " + enemy.item;
-    document.getElementById("enemyHPDisplay").innerHTML = "HP: "+ enemy.currentHealth + "/" + enemy.maxHealth;
-    document.getElementById("enemyHP").value = enemy.currentHealth;
-    document.getElementById("enemyHP").max = enemy.maxHealth;
-    logtxt = "Just another day at Wacken Open Air.";
-    log.innerHTML = logtxt;
-    logtxt = logtxt + ("<br>" + hero.name + " the " + hero.race + " will fight " + enemy.name + " the " + enemy.race + "!");
-    log.innerHTML = logtxt;
-    document.getElementById("logContainer").scrollTop = 9999999;
-    modalEnemy.style.display = "none";
- }
-
-changeEnemy = () => {
-    switch(document.getElementById("enemyName").value) {
-        case "Varg Vikernes":
-            document.getElementById("enemyRace").value = "Human";
-            document.getElementById("enemyItem").value = "Staff";
-            document.getElementById("enemyPhoto").src = "assets/enemy-varg-human-rest.jpg";
-            break;
-        case "Abbath":
-            document.getElementById("enemyRace").value = "Orc";
-            document.getElementById("enemyItem").value = "Sword";
-            document.getElementById("enemyPhoto").src = "assets/enemy-abbath-orc-rest.jpg";
-            break;
-            case "Aerendir":
-            document.getElementById("enemyRace").value = "Elf";
-            document.getElementById("enemyItem").value = "Bow";
-            document.getElementById("enemyPhoto").src = "assets/enemy-aerendir-elf-rest.jpg";
-            break;
-        case "Frost":
-            document.getElementById("enemyRace").value = "Vampire";
-            document.getElementById("enemyItem").value = "Boots";
-            document.getElementById("enemyPhoto").src = "assets/enemy-frost-vampire-rest.jpg";
-            break;
-      }
-}
-
-//Use this script to generate your character
-function Person(name, race, item) {
-    this.name = name;
-    this.race = race;
-    this.item = item;
-    this.currentHealth = 100;
-    this.maxHealth = 100;
-    this.min = 3;
-    this.maxDamage = 20;
-    this.maxHealing = 30;
-
-    this.heal = function(){};
-
-    this.damage = function(){};
-
-    this.totalDamage = this.damage();
-
-    this.displayChar = function(){
-        return console.log(`I am ${this.name} the ${this.race}! I wield a ${this.item} and my total health points are ${this.maxHealth}`);
-    };
-}
